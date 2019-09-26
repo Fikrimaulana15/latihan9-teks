645e130a823e2a0328861d909598dd4b5b519d2b
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-09-26 17:00:23 +0700

    selesai

---
+++ src/pbo2/pkg10118074/latihanteks/PBO210118074LatihanTeks.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihanteks;
+
+/**
+ *
+ * @author 
+ * NAMA : FikriMaulana
+ * KELAS : IF-2
+ * NIM : 10118074
+ */
+public class PBO210118074LatihanTeks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil " + newline +"char: " + tab + "berisi" + c);
+        
+        
+    }
+    
+}
